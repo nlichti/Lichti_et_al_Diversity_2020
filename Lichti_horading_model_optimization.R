786d7362aba0030f503636715961ec42bcccd307
Nate Lichti <nlichti@purdue.edu>
2020-06-06 17:33:25 -0400

    add GNUv3 license text

--- Lichti_horading_model_optimization.R
+++ Lichti_horading_model_optimization.R
@@ -3,7 +3,23 @@
 # Lichti et al. (submitted) Shade, cache-pilferage, and anti-predator behavior 
 #   in foragers may drive seed trait evolution in scatter-hoarded plants. 
 #   Diversity X:XX-XX.
-# Nathanael Lichti (nlichti@purdue.edu) - 06/06/2020 
+#
+# Copyright (C) 2020 Nathanael Lichti (nlichti@purdue.edu) 
+# Edited 06/06/2020
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+#
 # -----------------------------------------------------------------------------
 
 source('Lichti_hoarding_model_06062020-1600.R')
